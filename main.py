84e556359e708b6130312f40f358d93f598b9ed2
elenatuparova <elena.tuparova@gmail.com>
2019-04-14 13:17:56 +0300

    Added enemies file parameters

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from roles import Hero
 if __name__ == "__main__":
     hero = Hero(name="Bron", title="Dragonslayer", health=100, mana=100, mana_regeneration_rate=2)
 
-    map1 = Dungeon("dungeons/level1.txt", 'treasures/level1_treasures.json', hero)
-    map2 = Dungeon("dungeons/level2.txt", 'treasures/level2_treasures.json', hero)
-    map3 = Dungeon("dungeons/level3.txt", 'treasures/level3_treasures.json', hero)
+    map1 = Dungeon("dungeons/level1.txt", 'treasures/level1_treasures.json', 'enemies/level1_enemies.json', hero)
+    map2 = Dungeon("dungeons/level2.txt", 'treasures/level2_treasures.json', 'enemies/level2_enemies.json', hero)
+    map3 = Dungeon("dungeons/level3.txt", 'treasures/level3_treasures.json', 'enemies/level3_enemies.json', hero)
 
     map1.spawn()
 
