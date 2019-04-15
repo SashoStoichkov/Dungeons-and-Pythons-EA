d4dc8496f987ed7d622b9aa4ad13a427763d605c
elenatuparova <elena.tuparova@gmail.com>
2019-04-15 14:44:30 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -2,18 +2,14 @@ from dungeons_and_treasures import *
 from roles import Hero
 
 if __name__ == "__main__":
+
     hero = Hero(name="Bron", title="Dragonslayer", health=100, mana=100, mana_regeneration_rate=5)
 
-    map1 = Dungeon("dungeons/level1.txt", 'treasures/level1_treasures.json', 'enemies/level1_enemies.json', hero)
-    map2 = Dungeon("dungeons/level2.txt", 'treasures/level2_treasures.json', 'enemies/level2_enemies.json', hero)
-    map3 = Dungeon("dungeons/level3.txt", 'treasures/level3_treasures.json', 'enemies/level3_enemies.json', hero)
-
-    map1.spawn()
-
-    map2.spawn()
-
-    map3.spawn()
-
-    map1.move_hero()
-    map2.move_hero()
-    map3.move_hero()
+    levels = ["dungeons/level1.txt", "dungeons/level2.txt", "dungeons/level3.txt"]
+    treasures = ['treasures/level1_treasures.json', 'treasures/level2_treasures.json', 'treasures/level3_treasures.json']
+    enemies = ['enemies/level1_enemies.json', 'enemies/level2_enemies.json', 'enemies/level3_enemies.json']
+    
+    for index in range(len(levels)):
+        level = Dungeon(levels[index], treasures[index], enemies[index], hero)
+        level.spawn()
+        level.move_hero()
